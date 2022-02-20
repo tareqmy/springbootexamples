1bc962568525834f6da6cf7e71cf96004b19dc98
Tareq Mohammad Yousuf <tareq.y@gmail.com>
2022-02-20 21:48:07 +0600

    spring security with httpbasic and customauthentrypoint

---
+++ src/main/java/com/tareqmy/springbootexamples/configs/WebSecurityConfig.java
@@ -0,0 +1,49 @@
+package com.tareqmy.springbootexamples.configs;
+
+import com.tareqmy.springbootexamples.security.CustomBasicAuthenticationEntryPoint;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private CustomBasicAuthenticationEntryPoint authenticationEntryPoint;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        // authentication: http basic
+        http.authorizeRequests()
+                .antMatchers("/", "/index").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .httpBasic()
+                .authenticationEntryPoint(authenticationEntryPoint)
+                .and()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+
+        http.userDetailsService(userDetailsService());
+    }
+
+    @Override
+    public UserDetailsService userDetailsService() {
+        UserDetails user = User.withDefaultPasswordEncoder()
+                .username("user")
+                .password("password")
+                .roles("USER")
+                .build();
+
+        return new InMemoryUserDetailsManager(user);
+    }
+
+
+}
+
---
+++ src/main/java/com/tareqmy/springbootexamples/controllers/IndexController.java
@@ -0,0 +1,23 @@
+package com.tareqmy.springbootexamples.controllers;
+
+import com.tareqmy.springbootexamples.security.SecurityUtils;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class IndexController {
+
+    @GetMapping(value = {"/", "/index"}, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<String> getHome() {
+        String message = SecurityUtils.isAnonymousUser() ? "index" : "home";
+        return new ResponseEntity<>(message, HttpStatus.OK);
+    }
+
+    @GetMapping(value="/home", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<String> getUserHome() {
+        return new ResponseEntity<>("home", HttpStatus.OK);
+    }
+}
