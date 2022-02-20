8520b438bb928947be3b3b6ab858a9c74416284f
Tareq Mohammad Yousuf <tareq.y@gmail.com>
2022-02-20 19:21:23 +0600

    initial commit with only spring boot starter web

---
+++ src/main/java/com/tareqmy/springbootexamples/SpringbootexamplesApplication.java
@@ -0,0 +1,13 @@
+package com.tareqmy.springbootexamples;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootexamplesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootexamplesApplication.class, args);
+    }
+
+}
