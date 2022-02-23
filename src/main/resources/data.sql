01a1ab035c686384f5f3c92eef1ad1675ad8404f
Tareq Mohammad Yousuf <tareq.y@gmail.com>
2022-02-23 16:08:31 +0600

    adding system admin user

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,7 @@
 INSERT INTO users (id, username, password, first_name, last_name, role)
-VALUES (1, 'user', '$2a$12$Tq5Od/5n7TCioqKRFh8Sw.7SRHbSC0ZcEkyAhXRMidggQWw.2MUK6', 'user', 'name', 'ROLE_USER');
+VALUES (1, 'system', '$2a$12$bzZhkvjreqErTnDPvhWvm.R6Vz4QwivQqmopWkIHAOGMti5GPM2qC', 'system', 'name', 'ROLE_SYSTEM_ADMIN');
 INSERT INTO users (id, username, password, first_name, last_name, role)
 VALUES (2, 'admin', '$2a$12$aP.viZBtc6k5KVuJ.uaTce1R3wpP3XehfIBVzNL8dOEN6pd/jQmNK', 'admin', 'name', 'ROLE_ADMIN');
+INSERT INTO users (id, username, password, first_name, last_name, role)
+VALUES (3, 'user', '$2a$12$Tq5Od/5n7TCioqKRFh8Sw.7SRHbSC0ZcEkyAhXRMidggQWw.2MUK6', 'user', 'name', 'ROLE_USER');
+
